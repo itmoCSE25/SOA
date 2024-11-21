3ff8325d32e14d91d747ff1d93ae0272d2e478ea
Vlad <bekmvlad27@gmail.com>
2024-11-21 22:06:58 +0300

    убрал ssl до лучших времен

--- soa/src/main/java/com/yuiko/soa/config/WebConfiguration.java
+++ soa/src/main/java/com/yuiko/soa/config/WebConfiguration.java
@@ -1,18 +1,13 @@
 package com.yuiko.soa.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@EnableWebSecurity
+//@EnableWebSecurity
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
@@ -20,14 +15,14 @@ public class WebConfiguration implements WebMvcConfigurer {
         registry.addMapping("/**");
     }
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .requiresChannel(channel ->
-                        channel.anyRequest().requiresSecure())
-                .authorizeRequests(authorize ->
-                        authorize.anyRequest().permitAll())
-                .csrf(AbstractHttpConfigurer::disable)
-                .build();
-    }
+//    @Bean
+//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .requiresChannel(channel ->
+//                        channel.anyRequest().requiresSecure())
+//                .authorizeRequests(authorize ->
+//                        authorize.anyRequest().permitAll())
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .build();
+//    }
 }
