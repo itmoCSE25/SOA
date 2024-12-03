07749fcbcf3e8cf1f61f4b826cd8d3580c281cf0
Vlad <bekmvlad27@gmail.com>
2024-12-03 21:08:09 +0300

    add cors mappings

--- soa/src/main/java/com/yuiko/soa/config/WebConfiguration.java
+++ soa/src/main/java/com/yuiko/soa/config/WebConfiguration.java
@@ -1,28 +1,16 @@
-//package com.yuiko.soa.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//@EnableWebMvc
-////@EnableWebSecurity
-//public class WebConfiguration implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**").allowedMethods("*");
-//    }
-//
-////    @Bean
-////    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-////        return http
-////                .requiresChannel(channel ->
-////                        channel.anyRequest().requiresSecure())
-////                .authorizeRequests(authorize ->
-////                        authorize.anyRequest().permitAll())
-////                .csrf(AbstractHttpConfigurer::disable)
-////                .build();
-////    }
-//}
+package com.yuiko.soa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
